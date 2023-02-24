56cda072c28fccfcfe6582024366609e9b6ba4f1
DmitryDobr <63702962+DmitryDobr@users.noreply.github.com>
2023-02-24 19:32:22 +0300

    Create Combinatory.py

--- Combinatory.py
+++ Combinatory.py
@@ -18,6 +18,15 @@ def A(n,k):
     return (factorial(n) / (factorial(n-k)))
     
     
+# перестановки с повтором
+def nP(nList = []):
+    summ = 0
+    factmn = 1
+    for x in nList:
+        summ += x
+        factmn *= factorial(x)
+    
+    return factorial(summ) / factmn
     
 # cочетания с повтором
 def nC(n,k):
@@ -26,6 +35,3 @@ def nC(n,k):
 # размещения с повтором
 def nA(n,k):
     return n**k
-
-
-print(C(5,2))
